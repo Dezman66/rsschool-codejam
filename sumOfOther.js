f23399e4162a3c0115e5ddcf3c292c3ae23ba50e
Dezman66 <dezman66@gmail.com>
2018-10-28 18:00:22 +0300

    add file

---
+++ sumOfOther.js
@@ -0,0 +1,9 @@
+function sumOfOther (arr) {
+    let result = arr.reduce(function(sum, current) {
+        return sum + current;
+      });
+    let ans=arr.map(function(current){
+      return result-current
+    });
+  return ans
+}
