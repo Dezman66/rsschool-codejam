928a4fc658dae7cc87e99e7880ccdc4b4b724104
Dezman66 <dezman66@gmail.com>
2018-10-28 20:47:18 +0300

    add test

--- sumOfOther.js
+++ sumOfOther.js
@@ -1,9 +1,9 @@
-function sumOfOther (arr) {
+module.exports = function sumOfOther(arr) {
     let result = arr.reduce(function(sum, current) {
         return sum + current;
       });
     let ans=arr.map(function(current){
       return result-current
     });
-  return ans
+  return ans; 
 }
